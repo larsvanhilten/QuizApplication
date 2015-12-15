f97cb6c7b69b0fa5175dfb402d1708946e92fa71
Lars van Hilten <larsvh8@gmail.com>
2015-12-15 15:54:52 +0100

    0.1

---
+++ QuestionWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuizApplication
+{
+    /// <summary>
+    /// Interaction logic for QuestionWindow.xaml
+    /// </summary>
+    public partial class QuestionWindow : Window
+    {
+        public QuestionWindow()
+        {
+            InitializeComponent();
+            InitializeElements();
+        }
+
+        public void InitializeElements()
+        {
+            answerOne.Content = "2001";
+            answerTwo.Content = "2002";
+            answerThree.Content = "2003";
+        }
+    }
+
+
+
+}
